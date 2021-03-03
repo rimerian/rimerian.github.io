923df8c11a4bfac6593299bf239695737267fdb8
Roman <rimer93@gmail.com>
2021-03-03 19:31:08 +0300

    Resto SPA
    Added resto-project (SPA on React with routing and json)

---
+++ Projects/Resto/src/components/app/app.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {MainPage, CartPage, ItemPage} from '../pages';
+import AppHeader from '../app-header';
+
+import Background from './food-bg.jpg';
+import { Route, Switch } from 'react-router-dom';
+
+const App = () => {    
+    return (
+        <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
+            <AppHeader/>
+            <Switch>
+                <Route 
+                    path='/' 
+                    component={MainPage}
+                    exact/>
+                <Route 
+                    path='/cart' 
+                    component={CartPage}
+                    />
+                <Route
+                    path='/:id'
+                    component={ItemPage}
+                    />
+            </Switch>            
+        </div>
+    )
+}
+
+export default App;
---
+++ Projects/Resto/src/components/cart-table/cart-table.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './cart-table.scss';
+import {connect} from 'react-redux'
+import {deleteFromCart} from '../../actions';
+import WithRestoService from '../hoc';
+
+const CartTable = ({items, deleteFromCart, RestoService}) => {
+    if ( items.length === 0 ) {
+        return (<div className="cart__title"> Ваша корзина пуста :( </div>)
+    }
+    return (
+        <>
+            <div className="cart__title">Ваш заказ:</div>
+            <div className="cart__list">
+                {
+                    items.map(item => {
+                        const {title, url, price, id, qntty} = item;
+                        return (
+                            <div key={id} className="cart__item">
+                                <img src={url} className="cart__item-img" alt={title}></img>
+                                <div className="cart__item-title">{title}</div>
+                                <div className="cart__item-price">${price} * {qntty}</div>
+                                <div onClick={() => deleteFromCart(id)} className="cart__close">&times;</div>
+                             </div>
+                        );
+                    })
+                }               
+            </div>
+            <button 
+                onClick = {() => {RestoService.setOrder( generateOrder(items) ) }}
+                className = "order">
+                    Оформить заказ    
+            </button>
+        </>
+    );
+};
+
+const generateOrder = (items) => {
+    const newOrder = items.map(item => {
+        return {
+            id: item.id,
+            qntty: item.qntty
+        }
+    })
+    return newOrder;
+}
+
+const mapStateToProps = ({items}) => {
+    return {
+        items
+    }
+};
+
+const mapDispatchToProps = {
+    deleteFromCart
+};
+
+export default WithRestoService()(connect(mapStateToProps, mapDispatchToProps)(CartTable));
